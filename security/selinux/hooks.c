0f62375b3593b9ba8aee367cfefe7ea38ec4deab
Park Ju Hyung <qkrwngud825@gmail.com>
2020-08-09 07:05:42 +0900

    selinux: use stack on context to string conversions if possible (v2)
    For temporary usage such as logging, there's no need to allocate
    a dedicated memory.
    
    v2:
     - Fixed CONFIG_AUDIT support
     - Cleaned-up code style
     - Rebased after mainline backport
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3543,6 +3543,7 @@ static int selinux_inode_getsecurity(struct user_namespace *mnt_userns,
 {
 	u32 size;
 	int error;
+	char buf[SELINUX_LABEL_LENGTH];
 	char *context = NULL;
 	struct inode_security_struct *isec;
 
@@ -3564,21 +3565,31 @@ static int selinux_inode_getsecurity(struct user_namespace *mnt_userns,
 	 * in-core context value, not a denial.
 	 */
 	isec = inode_security(inode);
-	if (has_cap_mac_admin(false))
-		error = security_sid_to_context_force(isec->sid, &context,
-						      &size);
-	else
-		error = security_sid_to_context(isec->sid,
-						&context, &size);
+	if (!alloc)
+		context = buf;
+	if (has_cap_mac_admin(false)) {
+		if (alloc) {
+			error = security_sid_to_context_force(isec->sid, &context,
+							      &size);
+		} else {
+			error = security_sid_to_context_force_stack(isec->sid, &context,
+							      &size);
+		}
+	} else {
+		if (alloc) {
+			error = security_sid_to_context(isec->sid,
+							&context, &size);
+		} else {
+			error = security_sid_to_context_stack(isec->sid,
+							&context, &size);
+		}
+	}
 	if (error)
 		return error;
 	error = size;
-	if (alloc) {
+	if (alloc)
 		*buffer = context;
-		goto out_nofree;
-	}
-	kfree(context);
-out_nofree:
+
 	return error;
 }
 
@@ -5292,7 +5303,8 @@ static int selinux_socket_getpeersec_stream(struct socket *sock,
 					    unsigned int len)
 {
 	int err = 0;
-	char *scontext = NULL;
+	char buf[SELINUX_LABEL_LENGTH];
+	char *scontext = buf;
 	u32 scontext_len;
 	struct sk_security_struct *sksec = sock->sk->sk_security;
 	u32 peer_sid = SECSID_NULL;
@@ -5304,7 +5316,7 @@ static int selinux_socket_getpeersec_stream(struct socket *sock,
 	if (peer_sid == SECSID_NULL)
 		return -ENOPROTOOPT;
 
-	err = security_sid_to_context(peer_sid, &scontext,
+	err = security_sid_to_context_stack(peer_sid, &scontext,
 				      &scontext_len);
 	if (err)
 		return err;
@@ -5318,7 +5330,7 @@ static int selinux_socket_getpeersec_stream(struct socket *sock,
 out_len:
 	if (copy_to_sockptr(optlen, &scontext_len, sizeof(scontext_len)))
 		err = -EFAULT;
-	kfree(scontext);
+
 	return err;
 }
 
