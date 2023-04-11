25521fb5ca90ca9ad3859b6a636de67788c01250
Christoph Hellwig <hch@lst.de>
2023-04-11 19:14:51 +0200

    zram: rename __zram_bvec_read to zram_read_page
    __zram_bvec_read doesn't get passed a bvec, but always read a whole page.
    Rename it to make the usage more clear.
    
    Link: https://lkml.kernel.org/r/20230411171459.567614-10-hch@lst.de
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- google-modules/soc/gs/drivers/block/zram/zram_drv.c
+++ google-modules/soc/gs/drivers/block/zram/zram_drv.c
@@ -1244,8 +1244,8 @@ out:
 		~(1UL << ZRAM_LOCK | 1UL << ZRAM_UNDER_WB));
 }
 
-static int __zram_bvec_read(struct zram *zram, struct page *page, u32 index,
-				struct bio *bio, bool partial_io, bool access)
+static int zram_read_page(struct zram *zram, struct page *page, u32 index,
+			  struct bio *bio, bool partial_io, bool access)
 {
 	int ret;
 
@@ -1289,7 +1289,7 @@ static int zram_bvec_read(struct zram *zram, struct bio_vec *bvec,
 			return -ENOMEM;
 	}
 
-	ret = __zram_bvec_read(zram, page, index, bio, is_partial_io(bvec), access);
+	ret = zram_read_page(zram, page, index, bio, is_partial_io(bvec), access);
 	if (unlikely(ret))
 		goto out;
 
@@ -1331,7 +1331,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec,
 		if (!page)
 			return -ENOMEM;
 
-		ret = __zram_bvec_read(zram, page, index, bio, true, true);
+		ret = zram_read_page(zram, page, index, bio, true, true);
 		if (ret)
 			goto out;
 
