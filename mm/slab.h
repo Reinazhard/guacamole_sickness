fb1c4ba6043708ef79a3f9975623a7369640cb76
Catalin Marinas <catalin.marinas@arm.com>
2023-06-12 16:31:47 +0100

    mm/slab: simplify create_kmalloc_cache() args and make it static
    In the slab variant of kmem_cache_init(), call new_kmalloc_cache() instead
    of initialising the kmalloc_caches array directly.  With this,
    create_kmalloc_cache() is now only called from new_kmalloc_cache() in the
    same file, so make it static.  In addition, the useroffset argument is
    always 0 while usersize is the same as size.  Remove them.
    
    Link: https://lkml.kernel.org/r/20230612153201.554742-4-catalin.marinas@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Tested-by: Isaac J. Manjarres <isaacmanjarres@google.com>
    Cc: Alasdair Kergon <agk@redhat.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: Jerry Snitselaar <jsnitsel@redhat.com>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Jonathan Cameron <jic23@kernel.org>
    Cc: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Cc: Lars-Peter Clausen <lars@metafoo.de>
    Cc: Logan Gunthorpe <logang@deltatee.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Mike Snitzer <snitzer@kernel.org>
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Cc: Robin Murphy <robin.murphy@arm.com>
    Cc: Saravana Kannan <saravanak@google.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/slab.h
+++ mm/slab.h
@@ -303,9 +303,8 @@ enum track_item { TRACK_ALLOC, TRACK_FREE };
 /* Functions provided by the slab allocators */
 int __kmem_cache_create(struct kmem_cache *, slab_flags_t flags);
 
-struct kmem_cache *create_kmalloc_cache(const char *name, unsigned int size,
-			slab_flags_t flags, unsigned int useroffset,
-			unsigned int usersize);
+void __init new_kmalloc_cache(int idx, enum kmalloc_cache_type type,
+			      slab_flags_t flags);
 extern void create_boot_cache(struct kmem_cache *, const char *name,
 			unsigned int size, slab_flags_t flags,
 			unsigned int useroffset, unsigned int usersize);
