10a944ea288962f773f41f99b09bc29cd02f5376
peterz@infradead.org <peterz@infradead.org>
2023-09-21 12:45:13 +0200

    futex: Propagate flags into get_futex_key()
    Instead of only passing FLAGS_SHARED as a boolean, pass down flags as
    a whole.
    
    No functional change intended.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20230921105248.282857501@noisy.programming.kicks-ass.net

--- kernel/futex/pi.c
+++ kernel/futex/pi.c
@@ -934,7 +934,7 @@ int futex_lock_pi(u32 __user *uaddr, unsigned int flags, ktime_t *time, int tryl
 	to = futex_setup_timer(time, &timeout, flags, 0);
 
 retry:
-	ret = get_futex_key(uaddr, flags & FLAGS_SHARED, &q.key, FUTEX_WRITE);
+	ret = get_futex_key(uaddr, flags, &q.key, FUTEX_WRITE);
 	if (unlikely(ret != 0))
 		goto out;
 
@@ -1133,7 +1133,7 @@ retry:
 	if ((uval & FUTEX_TID_MASK) != vpid)
 		return -EPERM;
 
-	ret = get_futex_key(uaddr, flags & FLAGS_SHARED, &key, FUTEX_WRITE);
+	ret = get_futex_key(uaddr, flags, &key, FUTEX_WRITE);
 	if (ret)
 		return ret;
 
