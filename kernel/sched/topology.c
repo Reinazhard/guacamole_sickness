9f8f3a25ef33d093363a9c9389158ceff2f07433
Shrikanth Hegde <sshegde@linux.vnet.ibm.com>
2023-10-09 11:30:37 +0530

    sched/topology: Change behaviour of the 'sched_energy_aware' sysctl, based on the platform
    The 'sched_energy_aware' sysctl is available for the admin to disable/enable
    energy aware scheduling(EAS). EAS is enabled only if few conditions are
    met by the platform. They are, asymmetric CPU capacity, no SMT,
    schedutil CPUfreq governor, frequency invariant load tracking etc.
    A platform may boot without EAS capability, but could gain such
    capability at runtime. For example, changing/registering the cpufreq
    governor to schedutil.
    
    At present, though platform doesn't support EAS, this sysctl returns 1
    and it ends up calling build_perf_domains on write to 1 and
    NOP when writing to 0. That is confusing and un-necessary.
    
    Desired behavior would be to have this sysctl to enable/disable the EAS
    on supported platform. On non-supported platform write to the sysctl
    would return not supported error and read of the sysctl would return
    empty. So sched_energy_aware returns empty - EAS is not possible at this moment
    This will include EAS capable platforms which have at least one EAS
    condition false during startup, e.g. not using the schedutil cpufreq governor
    sched_energy_aware returns 0 - EAS is supported but disabled by admin.
    sched_energy_aware returns 1 - EAS is supported and enabled.
    
    User can find out the reason why EAS is not possible by checking
    info messages. sched_is_eas_possible returns true if the platform
    can do EAS at this moment.
    
    Signed-off-by: Shrikanth Hegde <sshegde@linux.vnet.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Tested-by: Pierre Gondois <pierre.gondois@arm.com>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lore.kernel.org/r/20231009060037.170765-3-sshegde@linux.vnet.ibm.com

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -215,6 +215,70 @@ static unsigned int sysctl_sched_energy_aware = 1;
 DEFINE_MUTEX(sched_energy_mutex);
 bool sched_energy_update;
 
+extern struct cpufreq_governor schedutil_gov;
+static bool sched_is_eas_possible(const struct cpumask *cpu_mask)
+{
+	bool any_asym_capacity = false;
+	struct cpufreq_policy *policy;
+	struct cpufreq_governor *gov;
+	int i;
+
+	/* EAS is enabled for asymmetric CPU capacity topologies. */
+	for_each_cpu(i, cpu_mask) {
+		if (rcu_access_pointer(per_cpu(sd_asym_cpucapacity, i))) {
+			any_asym_capacity = true;
+			break;
+		}
+	}
+	if (!any_asym_capacity) {
+		if (sched_debug()) {
+			pr_info("rd %*pbl: Checking EAS, CPUs do not have asymmetric capacities\n",
+				cpumask_pr_args(cpu_mask));
+		}
+		return false;
+	}
+
+	/* EAS definitely does *not* handle SMT */
+	if (sched_smt_active()) {
+		if (sched_debug()) {
+			pr_info("rd %*pbl: Checking EAS, SMT is not supported\n",
+				cpumask_pr_args(cpu_mask));
+		}
+		return false;
+	}
+
+	if (!arch_scale_freq_invariant()) {
+		if (sched_debug()) {
+			pr_info("rd %*pbl: Checking EAS: frequency-invariant load tracking not yet supported",
+				cpumask_pr_args(cpu_mask));
+		}
+		return false;
+	}
+
+	/* Do not attempt EAS if schedutil is not being used. */
+	for_each_cpu(i, cpu_mask) {
+		policy = cpufreq_cpu_get(i);
+		if (!policy) {
+			if (sched_debug()) {
+				pr_info("rd %*pbl: Checking EAS, cpufreq policy not set for CPU: %d",
+					cpumask_pr_args(cpu_mask), i);
+			}
+			return false;
+		}
+		gov = policy->governor;
+		cpufreq_cpu_put(policy);
+		if (gov != &schedutil_gov) {
+			if (sched_debug()) {
+				pr_info("rd %*pbl: Checking EAS, schedutil is mandatory\n",
+					cpumask_pr_args(cpu_mask));
+			}
+			return false;
+		}
+	}
+
+	return true;
+}
+
 void rebuild_sched_domains_energy(void)
 {
 	mutex_lock(&sched_energy_mutex);
@@ -232,6 +296,15 @@ static int sched_energy_aware_handler(struct ctl_table *table, int write,
 
 	if (write && !capable(CAP_SYS_ADMIN))
 		return -EPERM;
+
+	if (!sched_is_eas_possible(cpu_active_mask)) {
+		if (write) {
+			return -EOPNOTSUPP;
+		} else {
+			*lenp = 0;
+			return 0;
+		}
+	}
 
 	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
 	if (!ret && write) {
@@ -360,38 +433,12 @@ static bool build_perf_domains(const struct cpumask *cpu_map)
 	struct perf_domain *pd = NULL, *tmp;
 	int cpu = cpumask_first(cpu_map);
 	struct root_domain *rd = cpu_rq(cpu)->rd;
-	bool eas_check = false;
 
 	if (!sysctl_sched_energy_aware)
 		goto free;
 
-	/*
-	 * EAS is enabled for asymmetric CPU capacity topologies.
-	 * Allow vendor to override if desired.
-	 */
-	trace_android_rvh_build_perf_domains(&eas_check);
-	if (!per_cpu(sd_asym_cpucapacity, cpu) && !eas_check) {
-		if (sched_debug()) {
-			pr_info("rd %*pbl: CPUs do not have asymmetric capacities\n",
-					cpumask_pr_args(cpu_map));
-		}
+	if (!sched_is_eas_possible(cpu_map))
 		goto free;
-	}
-
-	/* EAS definitely does *not* handle SMT */
-	if (sched_smt_active()) {
-		pr_warn("rd %*pbl: Disabling EAS, SMT is not supported\n",
-			cpumask_pr_args(cpu_map));
-		goto free;
-	}
-
-	if (!arch_scale_freq_invariant()) {
-		if (sched_debug()) {
-			pr_warn("rd %*pbl: Disabling EAS: frequency-invariant load tracking not yet supported",
-				cpumask_pr_args(cpu_map));
-		}
-		goto free;
-	}
 
 	for_each_cpu(i, cpu_map) {
 		/* Skip already covered CPUs. */
