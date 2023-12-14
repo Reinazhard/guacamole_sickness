fb3661661a785c1d328c7ef7fc5f20df028bddda
Fabio M. De Francesco <fabio.maria.de.francesco@linux.intel.com>
2023-12-14 09:10:04 +0100

    mm/memory: replace kmap() with kmap_local_page()
    kmap() has been deprecated in favor of kmap_local_page().
    
    Therefore, replace kmap() with kmap_local_page() in mm/memory.c.
    
    There are two main problems with kmap(): (1) It comes with an overhead as
    the mapping space is restricted and protected by a global lock for
    synchronization and (2) it also requires global TLB invalidation when the
    kmap's pool wraps and it might block when the mapping space is fully
    utilized until a slot becomes available.
    
    With kmap_local_page() the mappings are per thread, CPU local, can take
    page-faults, and can be called from any context (including interrupts).
    It is faster than kmap() in kernels with HIGHMEM enabled.  The tasks can
    be preempted and, when they are scheduled to run again, the kernel virtual
    addresses are restored and still valid.
    
    Obviously, thread locality implies that the kernel virtual addresses
    returned by kmap_local_page() are only valid in the context of the callers
    (i.e., they cannot be handed to other threads).
    
    The use of kmap_local_page() in mm/memory.c does not break the
    above-mentioned assumption, so it is allowed and preferred.
    
    Link: https://lkml.kernel.org/r/20231215084417.2002370-1-fabio.maria.de.francesco@linux.intel.com
    Link: https://lkml.kernel.org/r/20231214081039.1919328-1-fabio.maria.de.francesco@linux.intel.com
    Signed-off-by: Fabio M. De Francesco <fabio.maria.de.francesco@linux.intel.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory.c
+++ mm/memory.c
@@ -5972,7 +5972,7 @@ int __access_remote_vm(struct mm_struct *mm, unsigned long addr, void *buf,
 			if (bytes > PAGE_SIZE-offset)
 				bytes = PAGE_SIZE-offset;
 
-			maddr = kmap(page);
+			maddr = kmap_local_page(page);
 			if (write) {
 				copy_to_user_page(vma, page, addr,
 						  maddr + offset, buf, bytes);
@@ -5981,8 +5981,7 @@ int __access_remote_vm(struct mm_struct *mm, unsigned long addr, void *buf,
 				copy_from_user_page(vma, page, addr,
 						    buf, maddr + offset, bytes);
 			}
-			kunmap(page);
-			put_page(page);
+			put_and_unmap_page(page, maddr);
 		}
 		len -= bytes;
 		buf += bytes;
