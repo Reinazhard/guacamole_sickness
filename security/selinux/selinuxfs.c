9124c41c2c86b71ecd9f99d5eb0caf5d8fa25145
Christian Göttsche <cgzones@googlemail.com>
2024-04-05 17:50:33 +0200

    selinux: pre-allocate the status page
    Since the status page is currently only allocated on first use, the
    sequence number of the initial policyload (i.e. 1) is not stored,
    leading to the observable sequence of 0, 2, 3, 4, ...
    
    Try to pre-allocate the status page during the initialization of the
    selinuxfs, so selinux_status_update_policyload() will set the sequence
    number.
    
    This brings the status page to return the actual sequence number for the
    initial policy load, which is also observable via the netlink socket.
    I could not find any occurrence where userspace depends on the actual
    value returned by selinux_status_policyload(3), thus the breakage should
    be unnoticed.
    
    Closes: https://lore.kernel.org/selinux/87o7fmua12.fsf@redhat.com/
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>
    [PM: trimmed 'reported-by' that was missing an email]
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -2164,6 +2164,12 @@ static int __init init_sel_fs(void)
 		return err;
 	}
 
+	/*
+	 * Try to pre-allocate the status page, so the sequence number of the
+	 * initial policy load can be stored.
+	 */
+	(void) selinux_kernel_status_page();
+
 	return err;
 }
 
