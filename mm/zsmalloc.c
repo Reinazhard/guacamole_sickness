5965f9e98740ad39a6785e6e456e89a545172482
Alex Shi <alexs@kernel.org>
2024-12-17 00:04:32 +0900

    mm/zsmalloc: add zpdesc memory descriptor for zswap.zpool
    Patch series "Add zpdesc memory descriptor for zswap.zpool", v9.
    
    This patch series introduces a new memory descriptor for zswap.zpool that
    currently overlaps with struct page for now.  This is part of the effort
    to reduce the size of struct page and to enable dynamic allocation of
    memory descriptors [1].
    
    This series does not bloat anything for zsmalloc and no functional change
    is intended (except for using zpdesc and folios).
    
    In the near future, the removal of page->index from struct page [2] will
    be addressed and the project also depends on this patch series.
    
    Thanks to everyone got involved in this series, especially, Alex who's
    been pushing it forward this year.
    
    [1] https://lore.kernel.org/linux-mm/ZvRKzKizOfEWBtJp@casper.infradead.org
    [2] https://lore.kernel.org/linux-mm/Z09hOy-UY9KC8WMb@casper.infradead.org
    
    This patch (of 18):
    
    The 1st patch introduces new memory descriptor zpdesc and renames
    zspage.first_page to zspage.first_zpdesc, with no functional change.
    
    We removed the comment about PG_owner_priv_1 since it is no longer used
    after commit a41ec880aa7b ("zsmalloc: move huge compressed obj from page
    to zspage").
    
    [rdunlap@infradead.org: fix function parameter kernel-doc notation]
      Link: https://lkml.kernel.org/r/20250111063305.911010-1-rdunlap@infradead.org
    [42.hyeyoo@gmail.com: rework comments a little bit]
    Link: https://lkml.kernel.org/r/20241216150450.1228021-1-42.hyeyoo@gmail.com
    Link: https://lkml.kernel.org/r/20241216150450.1228021-2-42.hyeyoo@gmail.com
    Originally-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>
    Signed-off-by: Alex Shi <alexs@kernel.org>
    Signed-off-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Acked-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Tested-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Alex Shi <alexs@kernel.org>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Vishal Moola (Oracle) <vishal.moola@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -13,22 +13,6 @@
  * Released under the terms of GNU General Public License Version 2.0
  */
 
-/*
- * Following is how we use various fields and flags of underlying
- * struct page(s) to form a zspage.
- *
- * Usage of struct page fields:
- *	page->private: points to zspage
- *	page->index: links together all component pages of a zspage
- *		For the huge page, this is always 0, so we use this field
- *		to store handle.
- *	page->page_type: first object offset in a subpage of zspage
- *
- * Usage of struct page flags:
- *	PG_private: identifies the first component page
- *	PG_owner_priv_1: identifies the huge component page
- *
- */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
@@ -66,6 +50,7 @@
 #include <linux/pagemap.h>
 #include <linux/fs.h>
 #include <linux/local_lock.h>
+#include "zpdesc.h"
 
 #define ZSPAGE_MAGIC	0x58
 
@@ -281,7 +266,7 @@ struct zspage {
 	};
 	unsigned int inuse;
 	unsigned int freeobj;
-	struct page *first_page;
+	struct zpdesc *first_zpdesc;
 	struct list_head list; /* fullness list */
 
 #ifdef CONFIG_ZPOOL
@@ -524,7 +509,7 @@ static inline void mod_zspage_inuse(struct zspage *zspage, int val)
 
 static inline struct page *get_first_page(struct zspage *zspage)
 {
-	struct page *first_page = zspage->first_page;
+	struct page *first_page = zpdesc_page(zspage->first_zpdesc);
 
 	VM_BUG_ON_PAGE(!is_first_page(first_page), first_page);
 	return first_page;
@@ -1073,7 +1058,7 @@ static void create_page_chain(struct size_class *class, struct zspage *zspage,
 		set_page_private(page, (unsigned long)zspage);
 		page->index = 0;
 		if (i == 0) {
-			zspage->first_page = page;
+			zspage->first_zpdesc = page_zpdesc(page);
 			SetPagePrivate(page);
 			if (unlikely(class->objs_per_zspage == 1 &&
 					class->pages_per_zspage == 1))
@@ -1439,8 +1424,7 @@ static unsigned long obj_malloc(struct zs_pool *pool,
 		/* record handle in the header of allocated chunk */
 		link->handle = handle | OBJ_ALLOCATED_TAG;
 	else
-		/* record handle to page->index */
-		zspage->first_page->index = handle | OBJ_ALLOCATED_TAG;
+		zspage->first_zpdesc->handle = handle | OBJ_ALLOCATED_TAG;
 
 	kunmap_local(vaddr);
 	mod_zspage_inuse(zspage, 1);
