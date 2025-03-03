7de0eabfe14860dde9455eacfb69d3ae1c3a7655
Eric Sandeen <sandeen@sandeen.net>
2025-03-03 11:12:13 -0600

    f2fs: factor out an f2fs_default_check function
    The current options parsing function both parses options and validates
    them - factor the validation out to reduce the size of the function and
    make transition to the new mount API possible, because under the new mount
    API, options are parsed one at a time, and cannot all be tested at the end
    of the parsing function.
    
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -679,7 +679,7 @@ static int parse_options(struct super_block *sb, char *options, bool is_remount)
 	int ret;
 
 	if (!options)
-		goto default_check;
+		return 0;
 
 	while ((p = strsep(&options, ",")) != NULL) {
 		int token;
@@ -1321,7 +1321,11 @@ static int parse_options(struct super_block *sb, char *options, bool is_remount)
 			return -EINVAL;
 		}
 	}
-default_check:
+	return 0;
+}
+
+static int f2fs_default_check(struct f2fs_sb_info *sbi)
+{
 #ifdef CONFIG_QUOTA
 	if (f2fs_check_quota_options(sbi))
 		return -EINVAL;
@@ -2400,6 +2404,10 @@ static int f2fs_remount(struct super_block *sb, int *flags, char *data)
 		goto restore_opts;
 	}
 #endif
+
+	err = f2fs_default_check(sbi);
+	if (err)
+		goto restore_opts;
 
 	/* flush outstanding errors before changing fs state */
 	flush_work(&sbi->s_error_work);
@@ -4554,6 +4562,10 @@ try_onemore:
 	}
 
 	err = parse_options(sb, options, false);
+	if (err)
+		goto free_options;
+
+	err = f2fs_default_check(sbi);
 	if (err)
 		goto free_options;
 
