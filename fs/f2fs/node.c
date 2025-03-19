c875b62b829e0bce279f970733f2587ba705ffd3
Colin Ian King <colin.i.king@gmail.com>
2025-03-19 11:30:10 +0000

    f2fs: remove redundant assignment to variable err
    The variable err is being assigned a value zero and then the following
    goto page_hit reassigns err a new value. The zero assignment is redundant
    and can be removed.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>
    [Jaegeuk Kim: clean up braces and if condition, suggested by Dan Carpenter]
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: GPL-2.0
 /*
  * fs/f2fs/node.c
@@ -1493,12 +1494,10 @@ repeat:
 		return ERR_PTR(-ENOMEM);
 
 	err = read_node_page(page, 0);
-	if (err < 0) {
+	if (err < 0)
 		goto out_put_err;
-	} else if (err == LOCKED_PAGE) {
-		err = 0;
+	if (err == LOCKED_PAGE)
 		goto page_hit;
-	}
 
 	if (parent)
 		f2fs_ra_node_pages(parent, start + 1, MAX_RA_NODE);
