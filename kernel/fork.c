8565cc1fdd577158a2d54fd042ff0d7eaf7a2c12
Peter Zijlstra <peterz@infradead.org>
2025-07-10 13:00:07 +0200

    futex: Use RCU-based per-CPU reference counting instead of rcuref_t
    The use of rcuref_t for reference counting introduces a performance bottleneck
    when accessed concurrently by multiple threads during futex operations.
    
    Replace rcuref_t with special crafted per-CPU reference counters. The
    lifetime logic remains the same.
    
    The newly allocate private hash starts in FR_PERCPU state. In this state, each
    futex operation that requires the private hash uses a per-CPU counter (an
    unsigned int) for incrementing or decrementing the reference count.
    
    When the private hash is about to be replaced, the per-CPU counters are
    migrated to a atomic_t counter mm_struct::futex_atomic.
    The migration process:
    - Waiting for one RCU grace period to ensure all users observe the
      current private hash. This can be skipped if a grace period elapsed
      since the private hash was assigned.
    
    - futex_private_hash::state is set to FR_ATOMIC, forcing all users to
      use mm_struct::futex_atomic for reference counting.
    
    - After a RCU grace period, all users are guaranteed to be using the
      atomic counter. The per-CPU counters can now be summed up and added to
      the atomic_t counter. If the resulting count is zero, the hash can be
      safely replaced. Otherwise, active users still hold a valid reference.
    
    - Once the atomic reference count drops to zero, the next futex
      operation will switch to the new private hash.
    
    call_rcu_hurry() is used to speed up transition which otherwise might be
    delay with RCU_LAZY. There is nothing wrong with using call_rcu(). The
    side effects would be that on auto scaling the new hash is used later
    and the SET_SLOTS prctl() will block longer.
    
    [bigeasy: commit description + mm get/ put_async]
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20250710110011.384614-3-bigeasy@linutronix.de

--- kernel/fork.c
+++ kernel/fork.c
@@ -1244,7 +1244,6 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 	RCU_INIT_POINTER(mm->exe_file, NULL);
 	mmu_notifier_subscriptions_init(mm);
 	init_tlb_flush_pending(mm);
-	futex_mm_init(mm);
 #if defined(CONFIG_TRANSPARENT_HUGEPAGE) && !USE_SPLIT_PMD_PTLOCKS
 	mm->pmd_huge_pte = NULL;
 #endif
@@ -1259,6 +1258,9 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 		mm->def_flags = 0;
 	}
 
+	if (futex_mm_init(mm))
+		goto fail_mm_init;
+
 	if (mm_alloc_pgd(mm))
 		goto fail_nopgd;
 
@@ -1272,6 +1274,8 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 fail_nocontext:
 	mm_free_pgd(mm);
 fail_nopgd:
+	futex_hash_free(mm);
+fail_mm_init:
 	free_mm(mm);
 	return NULL;
 }
@@ -1337,7 +1341,7 @@ void mmput(struct mm_struct *mm)
 }
 EXPORT_SYMBOL_GPL(mmput);
 
-#ifdef CONFIG_MMU
+#if defined(CONFIG_MMU) || defined(CONFIG_FUTEX_PRIVATE_HASH)
 static void mmput_async_fn(struct work_struct *work)
 {
 	struct mm_struct *mm = container_of(work, struct mm_struct,
