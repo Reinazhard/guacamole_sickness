18eb6ce322acdd1b4ad163cf15a1f5b891889f22
M. "Harumajati" Alfarozi <reinazhard@gmail.com>
2025-08-21 10:14:11 +0700

    max77759_charger: Don't return error from probe when debugfs creation fails

--- google-modules/bms/max77759_charger.c
+++ google-modules/bms/max77759_charger.c
@@ -3270,8 +3270,7 @@ static int max77759_charger_probe(struct i2c_client *client,
 
 	INIT_DELAYED_WORK(&data->mode_rerun_work, max77759_mode_rerun_work);
 
-	ret = dbg_init_fs(data);
-	if (ret < 0)
+	if (dbg_init_fs(data) < 0);
 		dev_err(dev, "Failed to initialize debug fs\n");
 
 	mutex_lock(&data->io_lock);
