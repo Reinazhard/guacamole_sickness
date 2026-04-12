6132caa57705005efa2b857bc144502963a164d4
M. "Harumajati" Alfarozi <reinazhard@gmail.com>
2026-04-13 06:29:55 +0700

    soc: gs: gsa: Fix compilation
    Signed-off-by: M. "Harumajati" Alfarozi <reinazhard@gmail.com>

--- google-modules/soc/gs/drivers/soc/google/gsa/gsa_core.c
+++ google-modules/soc/gs/drivers/soc/google/gsa/gsa_core.c
@@ -319,7 +319,7 @@ static int send_kdn_cmd(struct gsa_dev_state *s, u32 cmd,
 		return -EINVAL;
 	}
 
-	if (cb) {
+	if (cb && dst_buf) {
 		/* copy data to destination buffer */
 		memcpy(dst_buf, s->bb_va, cb);
 	}
@@ -461,7 +461,7 @@ static int send_sjtag_data_cmd(struct gsa_dev_state *s, u32 cmd,
 		return -EINVAL;
 	}
 
-	if (cb) {
+	if (cb && dst_buf) {
 		/* copy data to destination buffer */
 		memcpy(dst_buf, s->bb_va, cb);
 	}
