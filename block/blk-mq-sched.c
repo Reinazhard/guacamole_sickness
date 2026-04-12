17d4c503856ea5a07f155e26e87560c6dfc66144
M. "Harumajati" Alfarozi <reinazhard@gmail.com>
2026-04-12 16:11:39 +0700

    ANDROID: block: Fix KCSAN data race in blk_mq_sched_mark_restart_hctx

--- block/blk-mq-sched.c
+++ block/blk-mq-sched.c
@@ -23,7 +23,7 @@
  */
 void blk_mq_sched_mark_restart_hctx(struct blk_mq_hw_ctx *hctx)
 {
-	if (test_bit(BLK_MQ_S_SCHED_RESTART, &hctx->state))
+	if (data_race(test_bit(BLK_MQ_S_SCHED_RESTART, &hctx->state)))
 		return;
 
 	set_bit(BLK_MQ_S_SCHED_RESTART, &hctx->state);
