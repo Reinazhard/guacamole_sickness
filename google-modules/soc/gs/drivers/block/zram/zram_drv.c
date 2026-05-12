36c0486b3c1fbebfbbfacb350d2b0b0dd448302e
Richard Chang <richardycc@google.com>
2026-05-12 07:49:18 +0000

    zram: fix use-after-free in zram_writeback_endio
    A crash was observed in zram_writeback_endio due to a NULL pointer
    dereference in wake_up.  The root cause is a race condition between the
    bio completion handler (zram_writeback_endio) and the writeback task.
    
    In zram_writeback_endio, wake_up() is called on &wb_ctl->done_wait after
    releasing wb_ctl->done_lock.  This creates a race window where the
    writeback task can see num_inflight become 0, return, and free wb_ctl
    before zram_writeback_endio calls wake_up().
    
    CPU 0 (zram_writeback_endio)     CPU 1 (writeback_store)
    ============================     ============================
                                     zram_writeback_slots
                                       zram_submit_wb_request
                                       zram_submit_wb_request
                                       wait_event(wb_ctl->done_wait)
    spin_lock(&wb_ctl->done_lock);
    list_add(&req->entry, &wb_ctl->done_reqs);
    spin_unlock(&wb_ctl->done_lock);
    wake_up(&wb_ctl->done_wait);
                                       zram_complete_done_reqs
    spin_lock(&wb_ctl->done_lock);
    list_add(&req->entry, &wb_ctl->done_reqs);
    spin_unlock(&wb_ctl->done_lock);
                                       while (num_inflight) > 0)
                                         spin_lock(&wb_ctl->done_lock);
                                         list_del(&req->entry);
                                         spin_unlock(&wb_ctl->done_lock);
                                         // num_inflight becomes 0
                                         atomic_dec(num_inflight);
    
                                     // Leave zram_writeback_slots
                                     // Free wb_ctl
                                     release_wb_ctl(wb_ctl);
    // UAF crash!
    wake_up(&wb_ctl->done_wait);
    
    This patch fixes this race by using RCU.  By protecting wb_ctl with
    rcu_read_lock() in zram_writeback_endio and using kfree_rcu() to free it,
    we ensure that wb_ctl remains valid during the execution of
    zram_writeback_endio.
    
    Link: https://lore.kernel.org/20260512074918.2606208-1-richardycc@google.com
    Fixes: f405066a1f0d ("zram: introduce writeback bio batching")
    Signed-off-by: Richard Chang <richardycc@google.com>
    Suggested-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Suggested-by: Minchan Kim <minchan@kernel.org>
    Acked-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Martin Liu <liumartin@google.com>
    Cc: wang wei <a929244872@163.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: M. "Harumajati" Alfarozi <reinazhard@gmail.com>

--- google-modules/soc/gs/drivers/block/zram/zram_drv.c
+++ google-modules/soc/gs/drivers/block/zram/zram_drv.c
@@ -36,6 +36,7 @@
 #include <linux/kernel_read_file.h>
 #include <linux/swap.h>
 #include <soc/google/meminfo.h>
+#include <linux/rcupdate.h>
 
 #include "zram_drv.h"
 #include "zram_vh.h"
@@ -417,6 +418,7 @@ static int read_from_zspool_raw(struct zram *zram, struct page *page,
 				u32 index);
 static int read_from_zspool(struct zram *zram, struct page *page, u32 index);
 
+
 struct zram_wb_req {
 	unsigned long blk_idx;
 	struct page *page;
@@ -764,7 +766,7 @@ void release_wb_ctl(struct zram_wb_ctl *wb_ctl)
 		release_wb_req(req);
 	}
 
-	kfree(wb_ctl);
+	kfree_rcu(wb_ctl, rcu);
 }
 
 struct zram_wb_ctl *init_wb_ctl(struct zram *zram)
@@ -889,11 +891,13 @@ static void zram_writeback_endio(struct bio *bio)
 	struct zram_wb_ctl *wb_ctl = bio->bi_private;
 	unsigned long flags;
 
+	rcu_read_lock();
 	spin_lock_irqsave(&wb_ctl->done_lock, flags);
 	list_add(&req->entry, &wb_ctl->done_reqs);
 	spin_unlock_irqrestore(&wb_ctl->done_lock, flags);
 
 	wake_up(&wb_ctl->done_wait);
+	rcu_read_unlock();
 }
 
 static void zram_submit_wb_request(struct zram *zram,
